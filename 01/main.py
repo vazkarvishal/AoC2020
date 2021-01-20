6c5adb4d597fd8ae287f89ad690e528508252f2b
vishal.v@mmtdigital.co.uk <vishal.v@mmtdigital.co.uk>
2021-01-20 17:17:39 +0000

    refactoring

--- 01/main.py
+++ 01/main.py
@@ -1,7 +1,7 @@
 
 
 def check_addition_match(number:int, number2:int):
-  total = int(number) + int(number2)
+  total = number + number2
   # print (f"numbers checking are {number} ===== {number2} and total is {total}")
   if total == 2020:
     return True
@@ -13,16 +13,14 @@ def main_function():
   with open ('./input.txt') as file:
     for line in file:
       line = line.strip()
-      number_list.append(line)
-
-  number_list_2 = number_list 
+      number_list.append(int(line))
 
   for number in number_list:
-    for number2 in number_list_2:
+    for number2 in number_list:
       result_is_2020 = check_addition_match(number, number2)
       if result_is_2020:
         print(f"Numbers ${number} and ${number2} form a match!!")
-        final_result = int(number) * int(number2)
+        final_result = number * number2
         print(f"Final result is {final_result}")
 
         
