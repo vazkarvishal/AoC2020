d77746ac38e41c8f774dd7e53405c5500351d6d7
vishal.v@mmtdigital.co.uk <vishal.v@mmtdigital.co.uk>
2021-01-20 17:28:31 +0000

    part two

--- 01/main.py
+++ 01/main.py
@@ -1,12 +1,38 @@
 
 
-def check_addition_match(number:int, number2:int):
-  total = number + number2
+def check_addition_match(number:int, number2:int, number3:int = 0):
+  
+  total=0
+  if number3!=0:
+    total = number + number2 + number3
+  else:
+    total = number + number2 + number3
+  
   # print (f"numbers checking are {number} ===== {number2} and total is {total}")
   if total == 2020:
     return True
   else:
     return False
+
+def part_one(number_list):
+  for number in number_list:
+    for number2 in number_list:
+      result_is_2020 = check_addition_match(number, number2)
+      if result_is_2020:
+        # print(f"Numbers ${number} and ${number2} form a match!!")
+        final_result = number * number2
+        print(f"Part one final result is {final_result}")
+
+def part_two(number_list):
+  for number in number_list:
+    for number2 in number_list:
+      for number3 in number_list:
+        result_is_2020 = check_addition_match(number, number2, number3)
+        if result_is_2020:
+          # print(f"Numbers ${number} and ${number2} form a match!!")
+          final_result = number * number2 * number3
+          print(f"Part two final result is {final_result}")
+
 
 def main_function():
   number_list = []
@@ -14,16 +40,7 @@ def main_function():
     for line in file:
       line = line.strip()
       number_list.append(int(line))
-
-  for number in number_list:
-    for number2 in number_list:
-      result_is_2020 = check_addition_match(number, number2)
-      if result_is_2020:
-        print(f"Numbers ${number} and ${number2} form a match!!")
-        final_result = number * number2
-        print(f"Final result is {final_result}")
-
-        
-
+  part_one(number_list)
+  part_two(number_list)
 
 main_function()
